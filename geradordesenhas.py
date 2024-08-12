485d0e0096754609ca257f5abf6cdb400f84760a
Gustavo Paixão <166549657+gcpszn@users.noreply.github.com>
2024-08-12 20:04:16 -0300

    Gerador de Senhas
    Este script em Python gera uma senha aleatória com base nas preferências do usuário. O comprimento da senha é definido pelo usuário e deve estar entre 4 e 12 caracteres. O script utiliza a biblioteca random para selecionar caracteres aleatórios de um conjunto predefinido, que inclui letras minúsculas, números e caracteres especiais.
    
    Como Funciona
    Cabeçalho do Programa
    
    Exibe um cabeçalho formatado com o título "GERADOR DE SENHAS".
    Solicitação do Comprimento da Senha
    
    O usuário é solicitado a informar o número de caracteres desejado para a senha. O comprimento deve estar entre 4 e 12 caracteres.
    Geração da Senha
    
    Se o comprimento informado estiver dentro do intervalo permitido, o script gera uma senha aleatória.
    A senha é gerada selecionando caracteres aleatórios do conjunto definido e concatenando-os em uma string.
    Exibição da Senha
    
    A senha gerada é exibida para o usuário.
    Repetição ou Encerramento
    
    O usuário é perguntado se deseja tentar novamente com um novo comprimento de senha.
    Se a resposta não for 'S' (Sim), o programa é encerrado.
    Código

---
+++ geradordesenhas.py
@@ -0,0 +1,31 @@
+from random import choice # Importa a biblioteca para gerar números aleatórios
+
+print('-' * 20)
+print('GERADOR DE SENHAS'.center(20,' ')) # Cabeçalho do programa
+print('-' * 20)
+
+while True:
+	tam = int(input('Quantos caracteres são necessários [mínimo 4 a máximo 12 caracteres]: '))  # Solicita ao usuário o comprimento desejado para a senha
+	if 4 <= tam <= 12:	# Verifica se o comprimento está dentro do intervalo permitido
+		senha = '' # Inicializa a variável para armazenar a senha gerada
+		caracteres = 'qwertyuiopasdfghjklçzxcvbnm.789456123!@#$%*' # Conjunto de caracteres permitidos para a senha
+		
+		for digito in range(tam): # Executa o loop 'tam' vezes para gerar a senha com o número de caracteres desejado pelo usuário
+			aleatorio = choice(caracteres) # Seleciona um caractere aleatório 
+			senha += aleatorio # Adiciona o caractere à senha
+
+		print('-' * 30)
+		print(f'Senha gerada: {senha}') # Exibe a senha gerada
+		print('-' * 30)
+		break  # Sai do loop se a senha foi gerada com sucesso
+	else:
+		print('Número de caracteres inválido. Mínimo 4 e Máximo 12.')  # Informa ao usuário que o valor é inválido e solicita um novo 
+		
+	continuar = str(input('Deseja tentar novamente? [S/N]: ')).strip().upper() # Pergunta ao usuário se deseja tentar novamente
+	if continuar != 'S':
+		print('Encerrando o programa...')
+		break # Encerra o programa se o usuário não quiser continuar
+	
+	
+	
+		
